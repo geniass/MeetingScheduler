06411cccecdf070c02047503089e26542709bfd7
Ari Croock <aricroock@gmail.com>
2016-05-14 23:41:47 +0200

    Form allowing lecturers to book meetings

---
+++ cgi-bin/booking_form.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from jinja2 import Environment, FileSystemLoader
+import cgi
+import cgitb
+cgitb.enable()
+
+
+def populate_template(lecturer_id, date_time, is_student_booking=False):
+    loader = FileSystemLoader('../templates')
+    env = Environment(loader=loader)
+
+    context = {"lecturer_id": lecturer_id,
+               "date_time": date_time, "subject": "sdfsf"}
+    return env.get_template("booking_form.html").render(context)
+
+if __name__ == "__main__":
+    form = cgi.FieldStorage()
+    lecturer_id = form.getfirst("lecturer_id")
+    date_time = form.getfirst("date_time")
+
+    print("Content-type: text/html\n")
+    print(populate_template(lecturer_id, date_time))
