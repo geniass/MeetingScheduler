5a8303c08f937588d7cf09568b79acd63707dca3
547937 <weinyd@gmail.com>
2016-05-15 02:06:14 +0200

    new booking form for groups

--- cgi-bin/booking_form.py
+++ cgi-bin/booking_form.py
@@ -6,18 +6,21 @@ import cgitb
 cgitb.enable()
 
 
-def populate_template(lecturer_id, date_time, is_student_booking=False):
+def populate_template(lecturer_id, date_time, is_student_booking=False, is_group_booking = False, attendees = ''):
     loader = FileSystemLoader('../templates')
     env = Environment(loader=loader)
 
     context = {"lecturer_id": lecturer_id,
-               "date_time": date_time, "subject": "sdfsf"}
+               "date_time": date_time, "subject": "sdfsf", "is_group_booking": is_group_booking}
     return env.get_template("booking_form.html").render(context)
 
 if __name__ == "__main__":
     form = cgi.FieldStorage()
     lecturer_id = form.getfirst("lecturer_id")
     date_time = form.getfirst("date_time")
+
+    ################!!!!!!!! TO DO !!!!!!!!!!!!!!!! ##############################
+    # Need to read from the database to get the students who have booked and therefore whether it is a group meeting
 
     print("Content-type: text/html\n")
     print(populate_template(lecturer_id, date_time))
