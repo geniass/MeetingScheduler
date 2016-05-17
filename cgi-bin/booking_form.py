e5dfa23612ddc105aee9980a97e0ea41d3ec1dac
547937 <weinyd@gmail.com>
2016-05-17 15:14:18 +0200

    create student booking form

--- cgi-bin/booking_form.py
+++ cgi-bin/booking_form.py
@@ -6,9 +6,14 @@ import cgi
 import cgitb
 cgitb.enable()
 
+def str_to_bool(s):
+    if s == 'True':
+         return True
+    elif s == 'False':
+         return False
 
 def populate_template(lecturer_id, date_time,
-                      subject="", is_student_booking=False,
+                      subject="", is_student_booking = False,
                       is_group_booking=False, attendees=["547937", "597609"]):
     loader = FileSystemLoader('../templates')
     env = Environment(loader=loader)
@@ -16,6 +21,7 @@ def populate_template(lecturer_id, date_time,
     context = {"lecturer_id": lecturer_id,
                "date_time": date_time, "subject": subject,
                "is_group_booking": is_group_booking,
+               "is_student_booking": is_student_booking,
                "attendees": attendees}
     return env.get_template("booking_form.html").render(context)
 
@@ -23,8 +29,10 @@ if __name__ == "__main__":
     form = cgi.FieldStorage()
     lecturer_id = form.getfirst("lecturer_id")
     date_time = form.getfirst("date_time")
-
+    student = str_to_bool(form.getfirst("student_booking", False))
+    group = str_to_bool(form.getfirst("group_booking", False))
     m = meeting.get_by_date_time(lecturer_id, date_time)
+
     subject = m.subject if m else ""
 
     ################!!!!!!!! TO DO !!!!!!!!!!!!!!!! ##########################
@@ -32,4 +40,4 @@ if __name__ == "__main__":
     # therefore whether it is a group meeting
 
     print("Content-type: text/html\n")
-    print(populate_template(lecturer_id, date_time, subject=subject))
+    print(populate_template(lecturer_id, date_time, subject=subject, is_student_booking = student, is_group_booking = group))
