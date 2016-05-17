8d92a2ba10ad080d629796ae199ec7bc2d6a3c49
aliceyang16 <yangalice8@gmail.com>
2016-05-17 19:48:53 +0200

    Student Page using monthly calendar template

--- cgi-bin/monthly_calendar_template.py
+++ cgi-bin/monthly_calendar_template.py
@@ -3,9 +3,6 @@
 import os
 from datetime import datetime, timedelta
 from jinja2 import Environment, FileSystemLoader
-#import cgi
-#import cgitb
-#cgitb.enable()
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_ENVIRONMENT = Environment(
@@ -17,25 +14,16 @@ TEMPLATE_ENVIRONMENT = Environment(
 def render_template(template_filename, context):
 	return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
 
-def create_html(given_url):
-	url = given_url
-	context = {
-		'url': url
-	}
+def create_html():
 	html = render_template('monthly_calendar_template.html', context)
 	return html
 
 def main():
-	#form = cgi.FieldStorage()
-	#given_url = form.getfirst("startDate")
-
-	given_url = '/'
-	html = create_html(given_url)
+	html = create_html()
 	print("Content-type: text/html")
 	print("\n")
 	print(html)
 
-########################################
 
 if __name__ == "__main__":
 	main()
